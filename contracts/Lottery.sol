8df9e1516cc3dc20a84bf4b521785c4ecd3f87f9
tomek <01169316@wp.edu.pl>
2022-03-17 00:10:16 +0100

    third commit

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,12 +1,44 @@
-pragma solidity ^0.6.6;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery {
-    function enter() public {}
+    // we need to keep track of all the people that enter the lottery
+    address payable [] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPreceFeed;
 
-    function getEntranceFee() public{}
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50*(10**18);
+        ethUsdPreceFeed = AggregatorV3Interface(_priceFeedAddress);
+        
+    }
+
+    function enter() public payable {
+        // $50 minimum
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
+        players.push(payable(msg.sender));
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        (
+        /*uint80 roundID*/,
+        int256 price,
+        /*uint startedAt*/,
+        /*uint timeStamp*/,
+        /*uint80 answeredInRound*/
+        ) = ethUsdPreceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price)*(10**10); // we need 18 decimals
+        // $50, $2000/ETH
+        // 50/2000 but there cannot be any decimals
+        // 50 * 1000000 / 2000
+        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+        return costToEnter;
+    }
 
     function startLottery() public {}
-    
+
     function endLottery() public {}
 
 }
